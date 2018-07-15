ae032e854df5a267d9cdccaf8d0ca2cfa4743cde
kenigbolo <expensivestephen@hotmail.com>
2018-07-16 00:09:57 +0300

    add validations in models, add services folder to handle separated concerns and logic

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ApplicationRecord
     has_many :seasonal_rates
+    validates :title, :default_rate, presence: true
+    validates :default_rate, numericality: true
 end
--- app/models/seasonal_rate.rb
+++ app/models/seasonal_rate.rb
@@ -1,3 +1,6 @@
 class SeasonalRate < ApplicationRecord
   belongs_to :listing
+  validates :start_date, :end_date, :daily_rate, presence: true
+  validates :daily_rate, numericality: true
+  validates :listing, presence: true
 end
